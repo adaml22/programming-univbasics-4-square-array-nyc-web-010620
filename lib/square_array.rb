40d85fd2fbde0bec1941c045c403113ec991828f
Adam Longman <boardhead89@gmail.com>
2019-12-29 23:40:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   while counter < array.length do
     square_number = array[counter] * array[counter]
-    array.insert[counter + 1, square_number]
+    array.insert(counter + 1, square_number)
     array.delete_at[counter]
     counter += 1
   end
